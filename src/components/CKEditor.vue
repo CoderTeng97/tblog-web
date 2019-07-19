70e3353fc60c1d08cfd75625baa128bce36631aa
tenggang <1510545794@domain.com>
2019-07-19 13:38:37 +0800

    新增easyimage，预览

--- src/components/CKEditor.vue
+++ src/components/CKEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <textarea @fileUploadRequest="onFileUploadRequest($event)" name="blogEditor" id="blogEditor"></textarea>
+    <textarea name="blogEditor" id="blogEditor"></textarea>
   </div>
 </template>
 <script ></script>
@@ -31,37 +31,65 @@ export default {
     return {};
   },
   methods: {
-    onFileUploadRequest(){
+    onFileUploadRequest() {
       console.log(evt);
     }
   },
   async mounted() {
-    if(!window.CKEDITOR) {
-        await loadJs('https://cdn.bootcss.com/ckeditor/4.12.1/ckeditor.js')
-        await loadJs('https://cdn.bootcss.com/ckeditor/4.12.1/plugins/codesnippet/lib/highlight/highlight.pack.js')
+    if (!window.CKEDITOR) {
+      var blogEditor = document.getElementById('blogEditor');
+      blogEditor.style.display = 'none';
+      await loadJs("https://cdn.bootcss.com/ckeditor/4.12.1/ckeditor.js");
+       blogEditor.style.display = 'block';
     }
     var config = {
       //removePlugins:'image',
-      height:'600',
-      language:'zh-cn',
-      extraPlugins: 'codesnippet',
-      codeSnippet_theme: 'monokai_sublime',
-      codeSnippet_languages:{
-           javascript: 'JavaScript',
-           java: 'java'
+      title: "想你所写 ----TBLOG博客编辑器",
+      height: "600",
+      width:"50%",
+      language: "zh-cn",
+      extraPlugins: "codesnippet,easyimage,justify,link,preview,embedbase",
+      codeSnippet_theme: "monokai_sublime",
+      codeSnippet_languages: {
+        javascript: "JavaScript",
+        java: "java"
       },
-       on: {
-        fileUploadRequest: function(evt) {
-            console.log(evt.data.requestData.upload.file); // 'editor1'
-            //return 发送到服务器的所有数据对象
-        }
-      },
-      filebrowserImageUploadUrl:'http://localhost:8090/article/editor/upload'
-      
-    };
+    }
+      // on: {
+      //   paste: function(evt) {
+      //     for (var i = 0; i < evt.data.dataTransfer.getFilesCount(); i++) {
+      //       var file = evt.data.dataTransfer.getFile(i);
 
-    CKEDITOR.replace('blogEditor', config)
-    
+      //       if (CKEDITOR.fileTools.isTypeSupported(file, /image\/png/)) {
+      //         var loader = CKEDITOR.editor;
+      //         console.log(loader);
+
+              // loader.on("update", function() {
+              //   document.getElementById("uploadProgress").innerHTML =
+              //     loader.status;
+              // });
+
+              // loader.on("error", function() {
+              //   alert("Error!");
+              // });
+
+              // loader.loadAndUpload("http://upload.url/");
+
+              // evt.data.dataValue += "loading...";
+    //         }
+    //       }
+    //     }
+    //   }
+    //   //  on: {
+    //   //   fileUploadRequest: function(evt) {
+    //   //       console.log(evt.data.requestData.upload.file); // 'editor1'
+    //   //       //return 发送到服务器的所有数据对象
+    //   //   }
+    //   // },
+    //   //filebrowserImageUploadUrl:'http://localhost:8090/article/editor/upload'
+    // };
+    //console.log(ckeditor);
+    CKEDITOR.replace("blogEditor", config);
   }
 };
 </script>
