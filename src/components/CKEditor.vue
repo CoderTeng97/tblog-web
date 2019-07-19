93bc66f4e6b01bc57a348b44eb5e0e712a775f69
tenggang <1510545794@domain.com>
2019-07-19 17:46:21 +0800

    ckeditor内容图片及粘贴上传至阿里云OSS

--- src/components/CKEditor.vue
+++ src/components/CKEditor.vue
@@ -25,10 +25,14 @@ const loadJs = url =>
     script.src = url;
     document.body.appendChild(script);
   });
+import axios from "axios";
+import { async } from "q";
 export default {
   props: ["value"],
   data() {
-    return {};
+    return {
+      policy: null
+    };
   },
   methods: {
     onFileUploadRequest() {
@@ -36,60 +40,71 @@ export default {
     }
   },
   async mounted() {
+    console.log(this.policy);
     if (!window.CKEDITOR) {
-      var blogEditor = document.getElementById('blogEditor');
-      blogEditor.style.display = 'none';
-      await loadJs("https://cdn.bootcss.com/ckeditor/4.12.1/ckeditor.js");
-       blogEditor.style.display = 'block';
+      var blogEditor = document.getElementById("blogEditor");
+      blogEditor.style.display = "none";
+      //await loadJs("https://cdn.bootcss.com/ckeditor/4.12.1/ckeditor.js");
+      await loadJs("https://cdn.bootcss.com/ckeditor/4.11.4/ckeditor.js");
+      blogEditor.style.display = "block";
     }
     var config = {
-      //removePlugins:'image',
       title: "想你所写 ----TBLOG博客编辑器",
       height: "600",
-      width:"50%",
+      width: "50%",
       language: "zh-cn",
-      extraPlugins: "codesnippet,easyimage,justify,link,preview,embedbase",
+      extraPlugins:
+        "codesnippet,image2,justify,link,preview,embedbase,colorbutton",
+      removePlugins: "sourcedialog,",
       codeSnippet_theme: "monokai_sublime",
       codeSnippet_languages: {
         javascript: "JavaScript",
         java: "java"
       },
-    }
-      // on: {
-      //   paste: function(evt) {
-      //     for (var i = 0; i < evt.data.dataTransfer.getFilesCount(); i++) {
-      //       var file = evt.data.dataTransfer.getFile(i);
+      filebrowserImageUploadUrl: "/"
+    };
 
-      //       if (CKEDITOR.fileTools.isTypeSupported(file, /image\/png/)) {
-      //         var loader = CKEDITOR.editor;
-      //         console.log(loader);
-
-              // loader.on("update", function() {
-              //   document.getElementById("uploadProgress").innerHTML =
-              //     loader.status;
-              // });
-
-              // loader.on("error", function() {
-              //   alert("Error!");
-              // });
-
-              // loader.loadAndUpload("http://upload.url/");
-
-              // evt.data.dataValue += "loading...";
-    //         }
-    //       }
-    //     }
-    //   }
-    //   //  on: {
-    //   //   fileUploadRequest: function(evt) {
-    //   //       console.log(evt.data.requestData.upload.file); // 'editor1'
-    //   //       //return 发送到服务器的所有数据对象
-    //   //   }
-    //   // },
-    //   //filebrowserImageUploadUrl:'http://localhost:8090/article/editor/upload'
-    // };
-    //console.log(ckeditor);
     CKEDITOR.replace("blogEditor", config);
+    CKEDITOR.instances.blogEditor.on("fileUploadResponse", function(evt) {
+      console.log(evt.data);
+      // Prevent the default response handler.
+      evt.stop();
+      // Get XHR and response.
+      var data = evt.data;
+      data.url = data.fileLoader.ossPath;
+    });
+    async function getPolicy() {
+      return axios("/oss/wpolicy");
+    }
+    // const getPolicy = () => {
+    //   axios('/oss/wpolicy')
+    //       //this.policy = (await axios('/oss/wpolicy')).data.data
+    // }
+    CKEDITOR.instances.blogEditor.on(
+      "fileUploadRequest",
+      evt => {
+        var fileLoader = evt.data.fileLoader,
+          formData = new FormData(),
+          xhr = fileLoader.xhr;
+          getPolicy().then(val => {
+          let policy = val.data.data;
+          xhr.open("POST", policy.host, true);
+          console.log(policy);
+          for (let key of Object.keys(policy)) {
+            formData.append(key, policy[key]);
+          }
+          let ossPath = policy.dir + "/test";
+          formData.append("key", ossPath);
+          formData.append("file", fileLoader.file);
+          fileLoader.xhr.send(formData);
+          fileLoader.ossPath = policy.host + "/" + ossPath;
+        });
+        evt.stop();
+      },
+      null,
+      null,
+      5
+    );
   }
 };
 </script>
