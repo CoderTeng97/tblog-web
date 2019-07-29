fa65049a9c8ccdf0776c41cff8c560636ab52ba2
emily <862526226@qq.com>
2019-07-29 18:11:48 +0800

    login-html

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,42 +1,5 @@
 <template>
-  <div class="login">
-    <div class="logo">
-      <a href="/">
-        <img src="../../assets/logo.png" alt="Logo" />
-      </a>
-    </div>
-    <!-- form -->
-    <div class="main">
-      <el-form ref="form" :model="loginForm" class="login-form" autocomplete="on" label-position="left">
-        <h4 class="title">
-          <div class="normal-title">
-            <a class="active" href="/sign_in">登 录</a>
-            <b>·</b>
-            <a id="js-sign-up-btn" class href="/sign_up">注 册</a>
-          </div>
-        </h4>
-
-        <el-input 
-          :class="username"
-          style="padding-bottom:30px;"
-          v-model="loginForm.username" 
-          placeholder="请输入账户" >
-          <template slot="prepend">邮箱</template>
-        </el-input>
-
-        <el-input v-model="loginForm.password" placeholder="密码" style="padding-bottom:30px;">
-          <template slot="prepend">密码</template>
-        </el-input>
-
-        <el-button
-          :loading="loading"
-          type="primary"
-          style="width:100%;margin-bottom:30px;"
-          @click.native.prevent="handleLogin"
-        >登录</el-button>
-      </el-form>
-    </div>
-  </div>
+ <login-swiper></login-swiper>
 </template>
 
 <script>
@@ -49,18 +12,27 @@ export default {
     return {
       loginForm: {
         username: "",
-        password: ""
+        password: "",
+        role: ""
       },
       loading: false,
+      options: [
+        {
+          value: "",
+          label: "贡献值"
+        },
+        {
+          value: "",
+          label: "开发者"
+        }
+      ]
     };
   },
   methods: {
     handleLogin() {
       this.$router.push({ name: "release" });
     },
-    username() {
-      
-    }
+    username() {}
   }
 };
 </script>
@@ -68,62 +40,6 @@ export default {
 <style lang="stylus" scoped>
 @import '~@/assets/styles/main.styl';
 
-// 1vh = 1% * 视口高度  1rem = html 50px
-.login {
-  min-height: 100%;
-  width: 100%;
-  background-color: $loginbgColor;
-  overflow: hidden;
-  text-align: center;
-  font-size: 0.28rem;
-
-  .logo {
-    position: absolute;
-    top: 1.12rem;
-    margin-left: 1rem;
-  }
-
-  .main {
-    margin: 1.2rem auto 0;
-    padding: 1rem 1rem 0.6rem;
-    background-color: #fff;
-    border-radius: 0.08rem;
-    box-shadow: 0 0 0.16rem rgba(0, 0, 0, 0.1);
-    vertical-align: middle;
-    display: inline-block;
-  }
-  .login-form {
-      position: relative;
-      width: 520px;
-      max-width: 100%;
-      margin: 0 auto;
-      overflow: hidden;
-  }
-  .main .title {
-    margin: 0 auto 1rem;
-    padding: 10px;
-    font-weight: 400;
-    color: #969696;
-
-    .normal-title a {
-      font-size: 18px;
-      padding: 10px;
-      font-family: $logofont;
-      color: #ccc;
-    }
-
-    .normal-title b {
-      padding: 10px;
-      font-weight: 700;
-    }
-
-    .normal-title .active {
-      color: $logoColor;
-      font-weight: 700;
-      border-bottom: 2px solid #ea6f5a;
-    }
-  }
-}
 </style>
 
 
--- src/pages/login/components/LoginSwiper.vue
+++ src/pages/login/components/LoginSwiper.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="login-page sidebar-collapse">
-    <a class="login-in">in</a>
+    <a class="login-in">
+      <img src="../../../assets/logo.png">
+    </a>
     <div class="wrapper css-1g554ij">
-      <div class="swiper-bg">
-
-      </div>
+      <div class="loginleft-bg login-bg"></div>
     </div>
   </div>
 </template>
@@ -15,7 +15,9 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/main.styl';
+
 .login-in {
   cursor: pointer;
   display: block;
@@ -24,8 +26,9 @@ export default {
   top: 46px;
   z-index: 1;
 }
+
 .wrapper {
-  /* display: none; */
+  display: none;
   -webkit-box-align: center;
   align-items: center;
   -webkit-box-pack: center;
@@ -37,13 +40,31 @@ export default {
   min-height: 600px;
   height: 100%;
   overflow: hidden;
-  background: rgba(36, 40, 56, 0.65);
+  background:rgba(36, 40, 56, 0.65);
+
+  .loginleft-bg {
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 460px;
+    min-height: 600px;
+    height: 100%;
+    text-align: center;
+    overflow: hidden;
+    background: url("~@/assets/images/login-bg.jpg")  0% 0% / cover rgba(36, 40, 56, 0.65);
+  }
+
 }
 
 @media (min-width: 600px) {
   .login-in {
     left: 46px;
   }
+
   .css-1g554ij {
     position: fixed;
   }
@@ -52,6 +73,9 @@ export default {
 @media (min-width: 1076px) {
   .css-1g554ij {
     display: flex;
+  }
+  .login-bg {
+      display: flex;
   }
 }
 </style>
