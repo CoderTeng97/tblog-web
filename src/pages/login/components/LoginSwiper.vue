fd8992adaacd89f7aa2aa78414a0b8418720dda7
emily <862526226@qq.com>
2019-07-31 18:05:34 +0800

    login

--- src/pages/login/components/LoginSwiper.vue
+++ src/pages/login/components/LoginSwiper.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="login-page sidebar-collapse">
     <a class="login-in">
-      <img src="../../../assets/logo.png">
+      <img src="../../../assets/logo-smal.png" />
     </a>
     <div class="wrapper css-1g554ij">
-      <div class="loginleft-bg login-bg"></div>
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(item,index) of imgList" :key="index">
+          <img :src="item.imgUrl" class="swiper-img" />
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+      <!-- <div class="loginleft-bg login-bg"></div> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "LoginSwiper"
+  name: "LoginSwiper",
+  data() {
+    return {
+      swiperOption: {
+        loop: true,
+        autoplay: true,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets"
+        }
+      },
+      imgList: [
+        {
+          imgUrl: require("@/assets/images/molly-nix.jpg")
+        },
+        {
+          imgUrl: require("@/assets/images/andy-law.jpg")
+        }
+      ]
+    };
+  }
 };
 </script>
 
@@ -28,54 +56,20 @@ export default {
 }
 
 .wrapper {
-  display: none;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
   position: absolute;
   top: 0px;
   left: 0px;
   width: 460px;
   min-height: 600px;
   height: 100%;
+  // width: 100%;
+  height: 0;
   overflow: hidden;
-  background:rgba(36, 40, 56, 0.65);
+  background: rgba(36, 40, 56, 0.65);
 
-  .loginleft-bg {
-    -webkit-box-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 460px;
-    min-height: 600px;
-    height: 100%;
+  .swiper-img {
+    width: 100%;
     text-align: center;
-    overflow: hidden;
-    background: url("~@/assets/images/login-bg.jpg")  0% 0% / cover rgba(36, 40, 56, 0.65);
-  }
-
-}
-
-@media (min-width: 600px) {
-  .login-in {
-    left: 46px;
-  }
-
-  .css-1g554ij {
-    position: fixed;
-  }
-}
-
-@media (min-width: 1076px) {
-  .css-1g554ij {
-    display: flex;
-  }
-  .login-bg {
-      display: flex;
   }
 }
 </style>
